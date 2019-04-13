d7c616a9be23ba80485bae5b9cf960ed62bcf864
Guillaume Hivert <guillaume.hivert@outlook.com>
2019-04-13 19:15:05 +0200

    :art: Remove useless code

--- src/App.tsx
+++ src/App.tsx
@@ -15,22 +15,3 @@ export default (_props: Props) => {
     <AllChats/>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-})
