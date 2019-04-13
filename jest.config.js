a033d52208cc3ed9ba3ef39e77ed759b6ce747d0
Guillaume Hivert <guillaume.hivert@outlook.com>
2019-04-13 11:15:09 +0200

    :tada: First Commit

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/test/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)']
+}
